97e989a2705c8a1d600bf8989687336dc57a48b0
wesleyara <wes1gom@gmail.com>
2022-12-23 15:51:15 -0300

    feat: create addScripts util

---
+++ src/utils/addScripts.ts
@@ -0,0 +1,23 @@
+import { writeFileSync } from "fs";
+import { exec } from "shelljs";
+
+export const addScripts = async (
+  isRequiredCommitlint: boolean,
+  managerCommand: string,
+) => {
+  exec(`npm pkg set scripts.prepare="husky install"`);
+  exec(`${managerCommand} prepare`);
+
+  exec(`npx husky add .husky/pre-commit "npx --no-install lint-staged"`);
+
+  if (isRequiredCommitlint) {
+    exec(
+      'npx husky add .husky/pre-commit "npx --no -- commitlint --edit ${1}"',
+    );
+
+    writeFileSync(
+      "commitlint.config.js",
+      `module.exports = {extends: ["@commitlint/config-conventional"]};\r\n`,
+    );
+  }
+};
