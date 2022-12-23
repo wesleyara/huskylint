29a0869786215964f3fd5b220619dd67d0a9cc6c
wesleyara <wes1gom@gmail.com>
2022-12-23 15:50:35 -0300

    feat: create addDependencies util

---
+++ src/utils/addDependencies.ts
@@ -0,0 +1,34 @@
+import { exec } from "shelljs";
+import { delay } from "utils-react";
+
+import { IDependencies } from "../@types";
+import {
+  huskyDependencies,
+  lintStagedDependencies,
+  commitlintDependencies,
+} from "./constants";
+
+export const addDependencies = async (
+  isRequiredCommitlint: boolean,
+  managerMessage: string,
+) => {
+  const dependenciesObject: IDependencies = {
+    dependencies: [],
+    dev_dependencies: [],
+  };
+
+  dependenciesObject.dev_dependencies.push(
+    ...huskyDependencies,
+    ...lintStagedDependencies,
+  );
+
+  if (isRequiredCommitlint) {
+    dependenciesObject.dev_dependencies.push(...commitlintDependencies);
+  }
+
+  const dependencies = dependenciesObject.dev_dependencies.join(" ");
+  const addMessage = `${managerMessage} ${dependencies} -D`;
+
+  await delay(1000);
+  exec(addMessage);
+};
